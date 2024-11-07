d34fa66fa76470c82b920248580b5fb8446a92e2
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-11-07 13:58:23 +0100

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,3 +3,6 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
+
+timezone=$(cat /etc/timezone)
+main_domain=$(cat /etc/yunohost/current_host)
